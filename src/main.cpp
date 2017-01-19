5e447812dd3fbc3c42c85ac7f074e1141d1696a2
Vimmy <me@vimarshraina.com>
2017-01-19 11:44:10 +0000

    Now parse and prints functions appropriately

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,6 @@
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 
-//#include <iostream>
-
 using namespace clang;
 using namespace clang::tooling;
 using namespace clang::ast_matchers;
@@ -35,51 +33,424 @@ auto ClassDeclMatcher = cxxRecordDecl(isDefinition()).bind(classBindName);
 constexpr auto methodBindName = "method";
 auto MemberFunctionMatcher = cxxMethodDecl().bind(methodBindName);
 
+constexpr auto endl = "\n";
+
+const auto RemovePath = [](const std::string& str) {
+    auto name = str.find_last_of('\\');
+
+    if (name == std::string::npos) {
+        name = str.find_last_of('/');
+    }
+
+    if (name == std::string::npos) {
+        //ERROR!!
+    }
+
+    return str.substr(name + 1);
+};
+
+namespace Eegeo {
+    struct Type {
+        enum types {
+            Void,
+            Floating,
+            Integral,
+            Boolean,
+            Pointer,
+            FunctionPointer,
+            Array,
+            Struct,
+            String
+        };
+
+        std::string Keyword;
+        StringRef TypeName;
+        std::string Name;
+
+        static StringRef GetTypeName(types param) {
+            switch (param) {
+            case Void:
+                return "Void";
+
+            case Floating:
+                return "Floating";
+
+            case Integral:
+                return "Integral";
+
+            case Boolean:
+                return "Boolean";
+
+            case Pointer:
+                return "Pointer";
+
+            case FunctionPointer:
+                return "FunctionPointer";
+
+            case Array:
+                return "Array";
+
+            case Struct:
+                return "Struct";
+
+            case String:
+                return "String";
+
+            default:
+                //TODO(vim): Error
+                return "";
+            }
+        }
+    };
+
+    struct Method {
+        std::string Name;
+        Type ReturnType;
+        std::vector<Type> Params;
+    };
+
+    std::tuple<bool, std::string, Eegeo::Type::types> CheckBuiltinType(QualType CanonicalType, ASTContext& Context) {
+        std::string keyword;
+        Eegeo::Type::types returnTypeName = Eegeo::Type::Void;
+
+        bool typeFound = false;
+
+        if (!CanonicalType->isBuiltinType()) {
+            return { typeFound, keyword, returnTypeName };
+        }
+
+        typeFound = true;
+
+        keyword = CanonicalType.getAsString();
+
+        auto BT = dyn_cast<BuiltinType>(CanonicalType);
+
+        if (keyword == "_Bool") {
+            keyword = "bool";
+            returnTypeName = Eegeo::Type::Boolean;
+        }
+        else if (CanonicalType->isVoidType()) {
+            returnTypeName = Eegeo::Type::Void;
+        }
+        else if (CanonicalType->isIntegralType(Context)) {
+            returnTypeName = Eegeo::Type::Integral;
+        }
+        else if (CanonicalType->isFloatingType()) {
+            auto kind = BT->getKind();
+
+            //For future use case
+            if (kind == BuiltinType::Kind::Double || kind == BuiltinType::Kind::LongDouble)
+                returnTypeName = Eegeo::Type::Floating;
+            else
+                returnTypeName = Eegeo::Type::Floating;
+        }
+        else {
+            //TODO(vim): Error
+            llvm::errs() << "Unsupported Builtin Type Found!!" << endl;
+            typeFound = false;
+        }
+
+        return { typeFound, keyword, returnTypeName };
+    }
+
+    Type ProcessReturnType(QualType type, ASTContext& context) {
+        auto& OS = llvm::errs();
+
+        auto canonType = type.getDesugaredType(context).getCanonicalType();
+        canonType.removeLocalCVRQualifiers(Qualifiers::CVRMask);
+
+        auto unqual = type.getTypePtr();
+
+        std::string keyword;
+
+        Eegeo::Type::types returnTypeName = Eegeo::Type::Void;
+
+        if (unqual->isBuiltinType()) {
+
+            keyword = canonType.getAsString();
+
+            auto BT = dyn_cast<BuiltinType>(canonType);
+
+            if (keyword == "_Bool") {
+                keyword = "bool";
+                returnTypeName = Eegeo::Type::Boolean;
+            }
+            else if (unqual->isVoidType()) {
+                returnTypeName = Eegeo::Type::Void;
+            }
+            else if (unqual->isIntegralType(context)) {
+                returnTypeName = Eegeo::Type::Integral;
+            }
+            else if (unqual->isFloatingType()) {
+                auto kind = BT->getKind();
+
+                //For future use case
+                if (kind == BuiltinType::Kind::Double || kind == BuiltinType::Kind::LongDouble)
+                    returnTypeName = Eegeo::Type::Floating;
+                else
+                    returnTypeName = Eegeo::Type::Floating;
+            }
+            else {
+                //TODO(vim): Error
+                OS << "Unsupported Builtin Type Found!!" << endl;
+            }
+        }
+        else if (unqual->isFunctionPointerType()) {
+            keyword = canonType.getAsString();
+        }
+        else if (unqual->isPointerType()) {
+            auto PT = dyn_cast<PointerType>(canonType);
+
+            returnTypeName = Eegeo::Type::Pointer;
+
+            auto pointee = PT->getPointeeType();
+
+            if (pointee->isBuiltinType()) {
+                keyword = pointee.getAsString();
+            }
+            else if (pointee->isRecordType()) {
+                auto record = pointee->getAsCXXRecordDecl();
+
+                //if (!record->isPOD()) {
+                //    //TODO(vim): Error
+                //    OS << "Non POD Pointer Type Found!!" << endl;
+                //}
+
+                keyword = record->getName();
+            }
+            else {
+                //TODO(vim): Error
+                OS << "Unsupported Pointer Type Found!!" << endl;
+            }
+        }
+        else if (unqual->isRecordType()) {
+            auto RT = unqual->getAsCXXRecordDecl();
+
+            returnTypeName = Eegeo::Type::Struct;
+
+            if (RT->isPOD())
+                keyword = RT->getName();
+            else {
+                //TODO(vim): Error
+                OS << "Unsupported Value return type found!!" << endl;
+            }
+        }
+        else {
+            //TODO(vim): Error
+            OS << "Unsupported Param Type Found!!" << endl;
+        }
+
+        return { std::move(keyword), Eegeo::Type::GetTypeName(returnTypeName), "" };
+    }
+
+    Type ProcessParamType(QualType type, ASTContext& context) {
+        auto& OS = llvm::errs();
+
+        auto canonType = type.getDesugaredType(context).getCanonicalType();
+        canonType.removeLocalCVRQualifiers(Qualifiers::CVRMask);
+
+        auto unqual = type.getTypePtr();
+
+        std::string keyword;
+        std::string name;
+
+        Eegeo::Type::types returnTypeName = Eegeo::Type::Void;
+
+        if (unqual->isBuiltinType()) {
+
+            keyword = canonType.getAsString();
+
+            auto BT = dyn_cast<BuiltinType>(canonType);
+
+            if (keyword == "_Bool") {
+                keyword = "bool";
+                returnTypeName = Eegeo::Type::Boolean;
+            }
+            else if (unqual->isVoidType()) {
+                returnTypeName = Eegeo::Type::Void;
+                //TODO(vim): Error
+            }
+            else if (unqual->isIntegralType(context)) {
+                returnTypeName = Eegeo::Type::Integral;
+            }
+            else if (unqual->isFloatingType()) {
+                auto kind = BT->getKind();
+
+                //For future use case
+                if (kind == BuiltinType::Kind::Double || kind == BuiltinType::Kind::LongDouble)
+                    returnTypeName = Eegeo::Type::Floating;
+                else
+                    returnTypeName = Eegeo::Type::Floating;
+            }
+            else {
+                //TODO(vim): Error
+                OS << "Unsupported Builtin Type Found!!" << endl;
+            }
+        }
+        else if (unqual->isFunctionPointerType()) {
+            returnTypeName = Eegeo::Type::FunctionPointer;
+            keyword = canonType.getAsString();
+        }
+        else if (unqual->isPointerType()) {
+
+            if (const auto DT = dyn_cast<DecayedType>(unqual)) {
+                auto original = DT->getOriginalType();
+
+                if (const auto AT = dyn_cast<ArrayType>(original.getTypePtr())) {
+                    if (AT->getSizeModifier() == ArrayType::ArraySizeModifier::Normal) {
+                        const auto CAT = dyn_cast<ConstantArrayType>(AT);
+                        returnTypeName = Eegeo::Type::Array;
+                        keyword = original.getAsString();
+                    }
+                    else {
+                        //TODO(vim): Error
+                        OS << "Unbounded array types are not supported!!";
+                    }
+                }
+
+            }
+            else {
+
+                auto PT = dyn_cast<PointerType>(canonType);
+
+                returnTypeName = Eegeo::Type::Pointer;
+
+                auto pointee = PT->getPointeeType();
+
+                if (pointee->isBuiltinType()) {
+                    keyword = pointee.getAsString();
+                }
+                else if (pointee->isRecordType()) {
+                    auto record = pointee->getAsCXXRecordDecl();
+
+                    if (!record->isPOD()) {
+                        //TODO(vim): Error
+                        OS << "Non POD Pointer Type Found!!" << endl;
+                    }
+
+                    keyword = record->getName();
+                }
+                else {
+                    //TODO(vim): Error
+                    OS << "Unsupported Pointer Type Found!!" << endl;
+                }
+            }
+        }
+        else if (unqual->isRecordType()) {
+            auto RT = unqual->getAsCXXRecordDecl();
+
+            returnTypeName = Eegeo::Type::Struct;
+
+            if (RT->isPOD())
+                keyword = RT->getName();
+            else {
+                //TODO(vim): Error Example: std::string*
+                OS << "Unsupported Value return type found!!" << endl;
+            }
+        }
+        else if (unqual->isReferenceType()) {
+            auto RT = dyn_cast<ReferenceType>(unqual);
+            auto pointee = RT->getPointeeType();
+
+            if (pointee->isRecordType() && pointee->getAsCXXRecordDecl()->getDeclName().getAsString() == "basic_string") {
+                keyword = "char";
+                returnTypeName = Eegeo::Type::String;
+            }
+            else {
+                OS << "Unsupported reference type. Use a pointer instead!" << endl;
+            }
+
+        }
+        else {
+            //TODO(vim): Error
+            OS << "Unsupported Return Type Found!!" << endl;
+            type.dump();
+        }
+
+        return { std::move(keyword), Eegeo::Type::GetTypeName(returnTypeName), std::move(name) };
+    }
+}
+
 class MatchProcessor : public MatchFinder::MatchCallback {
     raw_ostream& OS{ llvm::errs() };
-    SmallString<20> ClassName;
+    raw_ostream& Dump{ llvm::errs() };
     SmallVector<SmallVector<SmallString<20>, 10>, 10> MemberFunctions;
+    StringRef SourceFile;
 
+    SmallString<20> ClassName;
+    SmallVector<Eegeo::Method, 10> Methods;
 public:
+    MatchProcessor(StringRef ref)
+        : SourceFile(std::move(ref)) {}
+
     void run(const MatchFinder::MatchResult &Result) override {
 
         const auto *classTree = Result.Nodes.getNodeAs<clang::CXXRecordDecl>(classBindName);
 
+        const auto getFileName = [](auto* ptr) {
+            std::string str = ptr->getASTContext().getSourceManager().getFilename(ptr->getLocation());
+            return RemovePath(str);
+        };
+
         if (classTree && ClassName.empty()) {
+
+            if (SourceFile != getFileName(classTree))
+                return;
+
             ClassName = classTree->getNameAsString();
         }
 
         if (const auto *methodTree = Result.Nodes.getNodeAs<clang::CXXMethodDecl>(methodBindName)) {
 
+            if (SourceFile != getFileName(methodTree))
+                return;
+
             if (isa<clang::CXXConstructorDecl>(methodTree)) { return; }
 
-            auto returnType = methodTree->getReturnType()->getUnqualifiedDesugaredType();
-            //auto typePtr = returnType.getTypePtr();
+            if (methodTree->getAccess() != AS_public) { return; }
 
-            auto ptr = methodTree->getReturnType()->getCanonicalTypeUnqualified();
+            auto returnType = methodTree->getReturnType();
 
-            //auto print = methodTree->getReturnType()->getCanonicalTypeInternal()->getTypeClassName();
+            auto retType = Eegeo::ProcessReturnType(returnType, methodTree->getASTContext());
 
-            //OS << methodTree->getReturnType()->decl <<  "========================\n";
-            returnType->dump();
+            //OS << methodTree->getDeclName() << " ";
 
-            if (!returnType->isBuiltinType()) {
-                //auto print = methodTree->getReturnType()->getTypeClassName();// returnType->getTypeClassName();
-                //OS << print << " " << methodTree->getDeclName() << "\n\n";
+            //***********************
+
+            std::vector<Eegeo::Type> params;
+
+            auto numParams = methodTree->getNumParams();
+
+            for (auto i = 0; i < numParams; ++i) {
+                auto paramType = Eegeo::ProcessParamType(methodTree->getParamDecl(i)->getType(), methodTree->getASTContext());
+                paramType.Name = methodTree->getParamDecl(i)->getDeclName().getAsString();
+                //OS << "\t" << paramType.Keyword << " [" << paramType.TypeName << "] " << methodTree->getParamDecl(i)->getDeclName() << endl;// paramType.Name << endl;
+
+                params.emplace_back(std::move(paramType));
             }
 
+            Methods.emplace_back(Eegeo::Method{ std::move(methodTree->getDeclName().getAsString()) , std::move(retType), std::move(params) });
+
+            //OS << endl << endl;
         }
 
     }
 
-    void printData() {
-        //OS << ClassName;
+    void Print() {
+        Dump << ClassName << endl << endl;
 
-        for (auto& function : MemberFunctions) {
+        for (auto& i : Methods) {
+            Dump << i.ReturnType.Keyword << " ";
 
+            Dump << i.Name << "(";
+
+            for (auto& j : i.Params) {
+                Dump << j.Keyword << " " << j.Name << ", ";
+            }
+
+            Dump << ")" << endl;
         }
 
-        OS << "\n";
     }
 };
 
@@ -88,14 +459,15 @@ int main(int argc, const char **argv) {
     ClangTool Tool(OptionsParser.getCompilations(),
         OptionsParser.getSourcePathList());
 
-    MatchProcessor Printer;
+    MatchProcessor Printer(RemovePath(OptionsParser.getSourcePathList()[0]));
     MatchFinder Finder;
+
     Finder.addMatcher(ClassDeclMatcher, &Printer);
     Finder.addMatcher(MemberFunctionMatcher, &Printer);
 
     auto ret = Tool.run(newFrontendActionFactory(&Finder).get());
 
-    Printer.printData();
+    Printer.Print();
 
     system("pause");
 
---
+++ test.cpp
@@ -0,0 +1,39 @@
+//BUILD DIR
+#include <string>
+
+namespace Eegeo {
+    namespace Api {
+
+        typedef void(*callback)(int, float);
+
+        //struct tag { int x; int y; int z; };
+        typedef const int t0;
+        typedef t0 t1;
+
+        class EegeoCameraApi {
+        public:
+            EegeoCameraApi();
+
+            callback OnUpdate(float deltaSeconds);
+
+            const bool SetView(float* configs, const std::string& name);
+
+            void SetViewToBounds(double* northEast,
+                float southWest[3],
+                int asdf);
+
+            t1 StopTransition();
+            EegeoCameraApi* TryInterruptTransition();
+
+            const long int CheckToNotifyZoomStart();
+
+            void SetEventCallback(callback cback);
+
+            float GetDistanceToInterest(double altitude, void(*)(int, float));
+
+            float* GetPitchDegrees(t1 something);
+
+        private:
+        };
+    }
+}
